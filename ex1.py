1138b01735af7c8eada87e177305109c4f741873
liam <mogoinpfo@gmail.com>
2022-11-06 17:28:35 +0200

    failure

--- ex1.py
+++ ex1.py
@@ -3,37 +3,50 @@ import scipy as sp
 from matplotlib import pyplot as plt
 
 # 1.
-x_min = -1.
-x_max = 1.
-h_approx = 1.e-4
-number = int((x_max-x_min) / h_approx)
-h = (x_max-x_min) / (number - 1)
-grid = np.linspace(x_min, x_max, num=number)
-print(grid[1] - grid[0] - h)
 
+
+def grid(x_min, x_max, h_approx):
+    number = int((x_max-x_min) / h_approx)
+    h = (x_max-x_min) / (number - 1)
+    grid = np.linspace(x_min, x_max, num=number)
+    return grid, h
+
+
+def d1(f, h, stencils, periodic=False):
+    rolling = []
+    for i in range(-stencils//2+1, stencils//2+1):
+        rolling.append([np.roll(f, -i)])
+    rolling = np.concatenate(rolling, axis=0)
+    if stencils == 5:
+        coefficients = np.transpose([np.array([1./12., -2./3., 0., 2./3., -1./12.])])
+    if stencils == 7:
+        coefficients = np.transpose([np.array([-1/60, 3/20, -3/4, 0, 3/4, -3/20, 1/60])])
+    if stencils == 9:
+        coefficients = np.transpose([np.array([1/280, -4/105, 1/5, -4/5, 0, 4/5, -1/5, 4/105, -1/280])])
+    if periodic:
+        return np.sum(rolling * coefficients, axis=0) / h
+    else:
+        return (np.sum(rolling * coefficients, axis=0) / h)[stencils//2:-stencils//2]
+
+
+def d2(f, h, stencils, periodic=False):
+    rolling = []
+    for i in range(-stencils//2+1, stencils//2+1):
+        rolling.append([np.roll(f, -i)])
+    rolling = np.concatenate(rolling, axis=0)
+    if stencils == 5:
+        coefficients = np.transpose([np.array([-1/12, 4/3, -5/2, 4/3, -1/12])])
+    if stencils == 7:
+        coefficients = np.transpose([np.array([1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90])])
+    if stencils == 9:
+        coefficients = np.transpose([np.array([-1/560, 8/315, -1/5, 8/5, -205/72, 8/5, -1/5, 8/315, -1/560])])
+    if periodic:
+        return np.sum(rolling * coefficients, axis=0) / h**2.
+    else:
+        return (np.sum(rolling * coefficients, axis=0) / h**2.)[stencils//2:-stencils//2]
 def f(x):
     return np.exp(x)
 
-def d1(f, stencils):
-    h = f[1] - f[0]
-    forward = []
-    backward = []
-    for i in range(stencils):
-        forward.append([np.roll(f, -i)])
-        backward.append([np.roll(f, i)])
-    forward = np.concatenate(forward, axis=0)[:, :-stencils]
-    backward = np.concatenate(backward, axis=0)[:, -stencils:]
-    if stencils == 5:
-        forward_coe = np.transpose([np.array([-25./12., 4., -3., 4./3., -1./4.])])
-    if stencils == 7:
-        forward_coe = np.transpose([np.array([-49./20., 6., -15./2., 20./3., -15./4., 6./5., -1./6.])])
-    if stencils == 9: #used wikipedia's online calculator...
-        forward_coe = np.transpose([np.array([-1.0187546202093276e+83, 2.9986995390864937e+83,
-                                                -5.247724193324426e+83, 6.996965591015617e+83,
-                                              -6.559655241526099e+83, 4.1981793545536496e+83,
-                                              -1.7492413977236302e+83, 4.2838564842079276e+82,
-                                              -4.685468029591042e+81])]) / 3.748374423901926e+82
-        # forward_coe = np.transpose([np.array([15/7, 8, -14, 56/3., -35./2., 56./5., -14./3., 8./7., -1./8])])
-    backward_coe = -forward_coe
-    return np.concatenate([np.sum(forward * forward_coe, axis=0), np.sum(backward * backward_coe, axis=0)], axis=0) / h
-
+x, h= grid(-1., 1., 1.e-3)
+s = 9
+print(d2(f(x), h, s) - f(x)[s//2:-s//2])
