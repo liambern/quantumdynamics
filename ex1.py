a2c9d5a800d011bc664fb3f23d5397beaf5be427
liam <mogoinpfo@gmail.com>
2022-11-13 11:29:31 +0200

    some text

--- ex1.py
+++ ex1.py
@@ -1,25 +1,22 @@
 import numpy as np
-import scipy as sp
 from matplotlib import pyplot as plt
 
-# 1.
 
-
-def grid(x_min, x_max, h_approx):
-    number = int((x_max-x_min) / h_approx)
+def grid(x_min, x_max, h_approx):  # creates a grid with an h close to the wanted h_approx
+    number = int((x_max - x_min) / h_approx)
     grid = np.linspace(x_min, x_max, num=number, endpoint=False)
     h = grid[1] - grid[0]
     return grid, h
 
 
-def der(f, h, stencils, periodic=False, order=1):
+def der(f, h, stencils, periodic=False, order=1): # finite differences
     rolling = []
-    for i in range(-stencils//2+1, stencils//2+1):
+    for i in range(-stencils // 2 + 1, stencils // 2 + 1):
         rolling.append([np.roll(f, -i)])
     rolling = np.concatenate(rolling, axis=0)
     if stencils == 5:
         if order == 1:
-            coefficients = np.transpose([np.array([1./12., -2./3., 0., 2./3., -1./12.])])
+            coefficients = np.transpose([np.array([1. / 12., -2. / 3., 0., 2. / 3., -1. / 12.])])
         if order == 2:
             coefficients = np.transpose([np.array([-1 / 12, 4 / 3, -5 / 2, 4 / 3, -1 / 12])])
     if stencils == 7:
@@ -35,28 +32,28 @@ def der(f, h, stencils, periodic=False, order=1):
             coefficients = np.transpose(
                 [np.array([-1 / 560, 8 / 315, -1 / 5, 8 / 5, -205 / 72, 8 / 5, -1 / 5, 8 / 315, -1 / 560])])
     if periodic:
-        return np.sum(rolling * coefficients, axis=0) / h**order
+        return np.sum(rolling * coefficients, axis=0) / h ** order
     else:
-        middle = (np.sum(rolling * coefficients, axis=0))[stencils//2:-stencils//2+1]
+        middle = (np.sum(rolling * coefficients, axis=0))[stencils // 2:-stencils // 2 + 1]
         forward = []
         backward = []
         for i in range(stencils):
             forward.append([np.roll(f, -i)])
             backward.append([np.roll(f, i)])
-        forward = np.concatenate(forward, axis=0)[:, :stencils//2]
-        backward = np.concatenate(backward, axis=0)[:, -stencils//2+1:]
+        forward = np.concatenate(forward, axis=0)[:, :stencils // 2]
+        backward = np.concatenate(backward, axis=0)[:, -stencils // 2 + 1:]
         if stencils == 5:
             if order == 1:
                 forward_coe = np.transpose([np.array([-25. / 12., 4., -3., 4. / 3., -1. / 4.])])
             if order == 2:
-                forward_coe = np.transpose([np.array([35/12, -26/3, 19/2, -14/3, 11/12])])
+                forward_coe = np.transpose([np.array([35 / 12, -26 / 3, 19 / 2, -14 / 3, 11 / 12])])
         if stencils == 7:
             if order == 1:
                 forward_coe = np.transpose(
                     [np.array([-49. / 20., 6., -15. / 2., 20. / 3., -15. / 4., 6. / 5., -1. / 6.])])
             if order == 2:
                 forward_coe = np.transpose(
-                    [np.array([-49/8, 29, -461/8, 62, -307/8, 13, -15/8])])
+                    [np.array([-49 / 8, 29, -461 / 8, 62, -307 / 8, 13, -15 / 8])])
         if stencils == 9:
             if order == 1:
                 forward_coe = np.transpose(
@@ -64,63 +61,64 @@ def der(f, h, stencils, periodic=False, order=1):
                                -14. / 3., 8. / 7., -1. / 8])])
             if order == 2:
                 forward_coe = np.transpose(
-                    [np.array([7.968898772174989e+67, -3.738158576058056e+68, 8.445829173551883e+68,-1.2107350749288884e+69,+1.1747318757695717e+69,-7.670608138130042e+68,+3.238398983443197e+68,-8.004788634475647e+67,+8.815953502150755e+66])]) / 1.3600369039952208e+67
-        backward_coe = forward_coe * (-1)**order
+                    [np.array(
+                        [7.968898772174989e+67, -3.738158576058056e+68, 8.445829173551883e+68, -1.2107350749288884e+69,
+                         +1.1747318757695717e+69, -7.670608138130042e+68, +3.238398983443197e+68,
+                         -8.004788634475647e+67, +8.815953502150755e+66])]) / 1.3600369039952208e+67
+        backward_coe = forward_coe * (-1) ** order
         forward_part = np.sum(forward * forward_coe, axis=0)
         backward_part = np.sum(backward * backward_coe, axis=0)
-        r = np.concatenate([forward_part, middle, backward_part], axis=0) / h**order
+        r = np.concatenate([forward_part, middle, backward_part], axis=0) / h ** order
         return r
 
 
-
-
-#2.
-
-def psi(x, p):
-    sigma0 = 0.5
+def psi(x, p, sigma0=0.5): # the wave packet
     x0 = 0.
-    return (sigma0*(2.*np.pi)**0.5)**(-0.5) * np.exp(-((x-x0)**2. / (4. * sigma0**2.)) + 1.j*p*x)
+    return (sigma0 * (2. * np.pi) ** 0.5) ** (-0.5) * np.exp(-((x - x0) ** 2. / (4. * sigma0 ** 2.)) + 1.j * p * x)
     # return np.exp(-x**2.)
 
 
-def barrier(x, x0, x1):
-    return np.heaviside(x-x0, 1) - np.heaviside(x-x1, 1)
+def barrier(x, x0, x1): #potential barrier of height 1 between x0 and x1
+    return np.heaviside(x - x0, 1) - np.heaviside(x - x1, 1)
 
 
-
-
-def f_dde(y, x, t, h):
+def f_dde(y, x, t, h): # the function to propagate in time in Runge-Kutta
     return -1.j * H(y, x, h)
 
 
-def energy(y, x, h):
-    return h*np.sum(np.conj(y) * H(y, x, h), axis=1)
-
-
-def runge_kutta(psi_0, x, h, dt):
+def runge_kutta(psi_0, x, h, dt): #4th order runge-kutta
     t0 = 0.
-    k1 = dt*f_dde(psi_0, x, t0, h)
-    k2 = dt*f_dde(psi_0 + k1/2, x, t0 + dt/2., h)
-    k3 = dt*f_dde(psi_0 + k2/2, x, t0 + dt/2., h)
-    k4 = dt*f_dde(psi_0 + k3, x, t0 + dt, h)
-    return psi_0 + (k1+2.*k2+2.*k3+k4)/6
+    k1 = dt * f_dde(psi_0, x, t0, h)
+    k2 = dt * f_dde(psi_0 + k1 / 2, x, t0 + dt / 2., h)
+    k3 = dt * f_dde(psi_0 + k2 / 2, x, t0 + dt / 2., h)
+    k4 = dt * f_dde(psi_0 + k3, x, t0 + dt, h)
+    return psi_0 + (k1 + 2. * k2 + 2. * k3 + k4) / 6
 
 
-#plot1
-def test(ff, dd1, dd2, h_list, s):
+def energy(y, x, h): #expectation value of the hamiltonian
+    return h * np.sum(np.conj(y) * H(y, x, h), axis=1)
+
+
+def test(ff, dd1, dd2, h_list, s, imag=False):
     d1_list = []
     d2_list = []
-    eps = 1.e-6
+    eps = 1.e-6 # to avoid (somewhat) numerical instabilities in the relative error
     for j in h_list:
         x, h = grid(-1, 1, j)
-        f = np.imag(ff(x))
-        d1 = np.imag(dd1(x))
-        d2 = np.imag(dd2(x))
-        err_d1 = abs((der(f, h, s, order=1)-d1)/(abs(d1)+eps))*100
-        err_d2 = abs((der(f, h, s, order=2)-d2)/(abs(d2)+eps))*100
-        d1_list.append(np.mean(err_d1[s//2:-s//2+1]))
-        d2_list.append(np.mean(err_d2[s//2:-s//2+1]))
+        if not imag:
+            f = np.real(ff(x))
+            d1 = np.real(dd1(x))
+            d2 = np.real(dd2(x))
+        else:
+            f = np.imag(ff(x))
+            d1 = np.imag(dd1(x))
+            d2 = np.imag(dd2(x))
+        err_d1 = abs((der(f, h, s, order=1) - d1) / (abs(d1) + eps)) * 100
+        err_d2 = abs((der(f, h, s, order=2) - d2) / (abs(d2) + eps)) * 100
+        d1_list.append(np.mean(err_d1[s // 2:-s // 2 + 1]))
+        d2_list.append(np.mean(err_d2[s // 2:-s // 2 + 1]))
     return d1_list, d2_list
+
 
 # h_list = np.logspace(-6, -1)
 # leg = []
@@ -146,22 +144,23 @@ def test(ff, dd1, dd2, h_list, s):
 # plt.savefig("img.pdf")
 
 
-
-
-
 x, h = grid(-5, 5, 5e-2)
+
+
 def H(f, x, h):
     # laplace = der(np.real(f), h, 5, periodic=True, order=2) + 1.j * der(np.imag(f), h, 5, periodic=True, order=2)
-    v = 1000*(barrier(x, 4, 5) + barrier(x, -5, -4))
-    return -0.5 * der(f, h, 9, periodic=True, order=2) + f*v
+    v = 1000 * (barrier(x, 4, 5) + barrier(x, -5, -4))
+    return -0.5 * der(f, h, 9, periodic=True, order=2) + f * v
+
+
 y = psi(x, 0)
 # y = np.sin(x)
 # plt.plot(x, y)
 # plt.plot(x, der(y, h, 5, order=2))
 # plt.show()
 # print(der(y, h, 5, order=1))
-y_list_1 = [[h*np.conj(y)*H(y, x, h)]]
-normalization_list_1 = [[h*np.conj(y)*y]]
+y_list_1 = [[h * np.conj(y) * H(y, x, h)]]
+normalization_list_1 = [[h * np.conj(y) * y]]
 num = len(y)
 p = 0.1
 plt.ion()
@@ -185,8 +184,8 @@ for i in range(steps):
         line3.set_ydata(np.imag(y))
         fig.canvas.draw()
         fig.canvas.flush_events()
-        y_list_1.append([h*np.conj(y)*H(y, x, h)])
-        normalization_list_1.append([h*np.conj(y)*y])
+        y_list_1.append([h * np.conj(y) * H(y, x, h)])
+        normalization_list_1.append([h * np.conj(y) * y])
     except KeyboardInterrupt:
         pass
 
