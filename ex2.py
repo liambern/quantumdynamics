399dc2d692dc2be49a3cdc766a844ce40e769227
liam <mogoinpfo@gmail.com>
2022-12-29 15:38:27 +0200

    basically done

--- ex2.py
+++ ex2.py
@@ -87,11 +87,32 @@ def initial_phi(x, c0, c1, k=1, m=1):
     return c0*f0 + c1*f1
 
 
+def phi_0(x, k, m):
+    return np.exp(-((k*m)**0.5/(2.*hbar))*x**2.) * ((k*m)**0.5/(np.pi*hbar))**(1./4.)
+
+
+def phi_1(x, k, m):
+    return np.exp(-((k*m)**0.5/(2.*hbar))*x**2.) * ((k*m)**0.5/(np.pi*hbar))**(1./4.)*x*(2.*(k*m)**0.5/hbar)**0.5
+
+
+def phi_2(x, k, m):
+    y = ((k*m)**0.5/hbar)**0.5 * x
+    return np.exp(-((k*m)**0.5/(2.*hbar))*x**2.) * ((k*m)**0.5/(np.pi*hbar))**(1./4.)*(2*y**2 - 1) / 2**0.5
+
+
+def phi_3(x, k, m):
+    y = ((k*m)**0.5/hbar)**0.5 * x
+    return np.exp(-((k*m)**0.5/(2.*hbar))*x**2.) * ((k*m)**0.5/(np.pi*hbar))**(1./4.) * (2*y**3 - 3*y) / 3**0.5
+
+
+def phi_4(x, k, m):
+    y = ((k*m)**0.5/hbar)**0.5 * x
+    return np.exp(-((k*m)**0.5/(2.*hbar))*x**2.) * ((k*m)**0.5/(np.pi*hbar))**(1./4.) * (16*y**4-48*y**2+12) / 384**0.5
+
+
 def exact_phi(x, t, c0, c1, k=1, m=1):
-    normalize = ((k*m)**0.5/(np.pi*hbar))**(1./4.)
-    expo = np.exp(-((k*m)**0.5/(2.*hbar))*x**2.)
-    f0 = normalize*expo
-    f1 = normalize*expo*x*(2.*(k*m)**0.5/hbar)**0.5
+    f0 = phi_0(x, k, m)
+    f1 = phi_1(x, k, m)
     in_exp_t = -1.j*((k / m) ** 0.5 / 2.)*t
     return c0*f0*np.exp(in_exp_t) + c1*f1*np.exp(3*in_exp_t)
 
@@ -131,20 +152,27 @@ def run(x_min, x_max, c0, c1, periodic=False, k=1, m=1, dx=5e-2, dk=0., tc=1., s
     normalization_list = [np.sum(h * np.conj(y) * y)]
     x_list = [np.sum(h * np.conj(y) * x * y)]
     p_list = [np.sum(h * np.conj(y) * P(y, x, h))]
+    c0_list = [np.abs(np.sum(h * np.conj(phi_0(x, k, m)) * y))**2.]
+    c1_list = [np.abs(np.sum(h * np.conj(phi_1(x, k, m)) * y))**2.]
+    c2_list = [np.abs(np.sum(h * np.conj(phi_2(x, k, m)) * y))**2.]
+    c3_list = [np.abs(np.sum(h * np.conj(phi_3(x, k, m)) * y))**2.]
+    c4_list = [np.abs(np.sum(h * np.conj(phi_4(x, k, m)) * y))**2.]
+
 
 
     if plot:
         fig = plt.figure()
         ax = fig.add_subplot(111)
         title = 'c0={c0}, c1={c1}, duration={t_run}'.format(c0=complex(round(c0.real, 5), round(c0.imag, 5)), c1=complex(round(c1.real, 5), round(c1.imag, 5)), t_run=str(t_max))
+        # title = 'Ψ_exact - Ψ_numerical'
         fig.suptitle(title)
-        ax.set_ylim([-1, 1])
+        ax.set_ylim([-2e-5, 2e-5])
         ax.set_xlim([x[0], x[-1]])
         ax.grid(True, which='both', ls='--')
         ax.axhline(y=0, color='k', alpha=0.75)
         ax.set_axisbelow(True)
         ax.set_xlabel("X")
-        # y_err = exact_phi(x, time, c0=c0, c1=c1, k=k, m=m) - y
+        # y_err = exact_phi(x, time_list[0], c0=c0, c1=c1, k=k, m=m) - y
         line1, = ax.plot(x, np.abs(y), label='|Ψ(x)|')
         line2, = ax.plot(x, np.real(y), label='Re(Ψ(x))')
         line3, = ax.plot(x, np.imag(y), label='Imag(Ψ(x))')
@@ -153,7 +181,7 @@ def run(x_min, x_max, c0, c1, periodic=False, k=1, m=1, dx=5e-2, dk=0., tc=1., s
         frame_skip = 4  # frame spacings to not plot, for memory reasons
 
         def animate(i):
-            for k in range(frame_skip):
+            for kkk in range(frame_skip):
                 y = runge_kutta(y_list[0], x, h, dt, time_list[0])
                 time_list[0] += dt
                 y_list[0] = y
@@ -161,16 +189,16 @@ def run(x_min, x_max, c0, c1, periodic=False, k=1, m=1, dx=5e-2, dk=0., tc=1., s
                 normalization_list.append(np.sum(h * np.conj(y) * y))
                 x_list.append(np.sum(h * np.conj(y) * x * y))
                 p_list.append(np.sum(h * np.conj(y) * P(y, x, h)))
-            # y_err = exact_phi(x, time, c0=c0, c1=c1, k=k, m=m) - y
+            # y_err = exact_phi(x, time_list[0], c0=c0, c1=c1, k=k, m=m) - y
             line1.set_ydata(np.abs(y))
             line2.set_ydata(np.real(y))
             line3.set_ydata(np.imag(y))
             return line1, line2, line3
         ani = FuncAnimation(fig, animate, frames=int((t_max / dt) / frame_skip), blit=True)
-        ani.save("max={max},min={min},c0={c0},c1={c1},periodic={periodic},error.gif".format(max=str(x_max), min=str(x_min), c0=str(c0)[:5], c1=str(c1)[:5],
+        ani.save("four.gif".format(max=str(x_max), min=str(x_min), c0=str(c0)[:5], c1=str(c1)[:5],
                 periodic=str(periodic)), dpi=250, writer=PillowWriter(fps=50))
     else:
-        for k in range(int(t_max / dt)):
+        for i in range(int((t_max / dt))):
             y = runge_kutta(y_list[0], x, h, dt, time_list[0])
             time_list[0] += dt
             y_list[0] = y
@@ -178,38 +206,68 @@ def run(x_min, x_max, c0, c1, periodic=False, k=1, m=1, dx=5e-2, dk=0., tc=1., s
             normalization_list.append(np.sum(h * np.conj(y) * y))
             x_list.append(np.sum(h * np.conj(y) * x * y))
             p_list.append(np.sum(h * np.conj(y) * P(y, x, h)))
+            c0_list.append(np.abs(np.sum(h * np.conj(phi_0(x, k, m)) * y)) ** 2.)
+            c1_list.append(np.abs(np.sum(h * np.conj(phi_1(x, k, m)) * y)) ** 2.)
+            c2_list.append(np.abs(np.sum(h * np.conj(phi_2(x, k, m)) * y)) ** 2.)
+            c3_list.append(np.abs(np.sum(h * np.conj(phi_3(x, k, m)) * y)) ** 2.)
+            c4_list.append(np.abs(np.sum(h * np.conj(phi_4(x, k, m)) * y)) ** 2.)
 
-    energies = np.real(energy_list)
+
+    energy_error = np.real(energy_list)
     normalization = np.real(normalization_list)
     mean_x = np.real(x_list)
     mean_p = np.real(p_list)
     # exact_energy = c0**2. * 0.5 + c1**2. * 1.5
-    exact_energy = energies[0]
-    energy_error = 100. * np.abs((exact_energy - energies) / exact_energy)
+    # exact_energy = energies[0]
+    # energy_error = 100. * np.abs((exact_energy - energies) / exact_energy)
     normalization_error = 100. * np.abs((1. - normalization))
-
-    return energy_error, normalization_error, mean_x, mean_p
+    cs_lists = [c0_list, c1_list, c2_list, c3_list, c4_list]
+    return energy_error, normalization_error, mean_x, mean_p, cs_lists
 
 
 cases0 = [0., 0.4, 1/2**0.5, 1/2**0.5, np.exp(-0.1*np.pi*1.j)/2**0.5, (1-0.4**2.)**0.5, 1.]
 cases1 = [1., (1-0.4**2.)**0.5, 1/2**0.5, -1/2**0.5, 1/2**0.5, 0.4, 0.]
-for i in range(len(cases0)):
-    energy_error, normalization_error, mean_x, mean_p = run(-5, 5, cases0[i], cases1[i], periodic=False, k=1, m=1, dx=5e-2, dt=0.001, t_max=5, plot=True)
+t_max = 6.5
+# for i in range(len(cases0)):
+#     energy_error, normalization_error, mean_x, mean_p, _ = run(-5, 5, cases0[i], cases1[i], periodic=False, k=1, m=1, dx=5e-2, dt=0.001, t_max=t_max, plot=True)
+#     steps = len(energy_error) - 1
+#     fig, axs = plt.subplots(2)
+#     axs[0].semilogy(np.arange(steps + 1), energy_error, label='With respect to the exact value')
+#     axs[0].set_ylabel("Energy")
+#
+#     axs[1].semilogy(np.arange(steps + 1), normalization_error, label='Periodic boundary, p=0')
+#     axs[1].set_ylabel("Normalization")
+#
+#     axs[0].legend()
+#     fig.suptitle("Relative errors [%], dt=0.001")
+#     plt.xlabel("Time steps [dt]")
+#     plt.savefig(str(i)+"errors.svg")
+#     plt.clf()
+#     plt.scatter(mean_p, mean_x, c=t_max*np.arange(len(mean_p))/(len(mean_p)-1), vmin=0, vmax=t_max, marker='.')
+#     plt.ylabel("<p>(t)")
+#     plt.xlabel("<x>(t)")
+#     plt.title("Phase-space sampling")
+#     cbar = plt.colorbar()
+#     cbar.set_label("Time")
+#     plt.savefig(str(i)+"five.svg")
+
+#part 2
+t_max = 10
+for i in range(1):
+    energy_error, normalization_error, mean_x, mean_p, cs_lists = run(-5, 5, 1., 0., periodic=False, k=1, m=1, dx=5e-2, dt=0.001, dk=1, tc=5, sigma=1, t_max=t_max, plot=False)
     steps = len(energy_error) - 1
     fig, axs = plt.subplots(2)
-    axs[0].semilogy(np.arange(steps + 1), energy_error, label='With respect to the exact value')
+    axs[0].plot(np.arange(steps + 1), energy_error, label='With respect to the exact value')
     axs[0].set_ylabel("Energy")
 
     axs[1].semilogy(np.arange(steps + 1), normalization_error, label='Periodic boundary, p=0')
-    axs[1].set_ylabel("Normalization")
+    axs[1].set_ylabel("Normalization Error [%]")
 
-    axs[0].legend()
-    fig.suptitle("Relative errors [%], dt=0.001")
+    fig.suptitle("Energy and relative error of the normalization], dt=0.001")
     plt.xlabel("Time steps [dt]")
-    plt.savefig(str(i)+"errors.svg")
+    plt.savefig(str(i)+"part2errors.svg")
     plt.clf()
-    plt.scatter(mean_p, mean_x)
-    plt.ylabel("<p>(t)")
-    plt.xlabel("<x>(t)")
-    plt.title("Phase-space sampling over 5 time units")
-    plt.savefig(str(i)+"five.svg")
+    for ci in range(len(cs_lists)):
+        plt.semilogy(np.arange(steps + 1), cs_lists[ci], label='|C'+str(ci)+ '|^2')
+    plt.legend()
+    plt.savefig(str(i) + "population.svg")
